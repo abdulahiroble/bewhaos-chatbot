3643d95037f36a751135f5b620334b77bff0dfb0
abdulahi roble <abdulahi_2610@hotmail.com>
2022-08-02 13:41:01 +0200

    Inital setup

---
+++ components/Chat.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Chatbot from 'react-chatbot-kit'
+import config from '../config/config'
+import ActionProvider from '../config/ActionProvider'
+import MessageParser from '../config/MessageParser'
+
+const Chat = () => {
+    return (
+        <Chatbot actionProvider={ActionProvider} messageParser={MessageParser} config={config} />
+    )
+}
+
+export default Chat
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+// import Chatbot from 'react-chatbot-kit'
+// import config from '../config/config'
+// import ActionProvider from '../config/config'
+// import MessageParser from '../config/config'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+  return <Component {...pageProps} >
+    {/* <Chatbot actionProvider={ActionProvider} messageParser={MessageParser} config={config} /> */}
+  </Component>
 }
 
 export default MyApp
