2f035a0475568548892fc1c4176d53282cc6ca9f
abdulahi roble <abdulahi_2610@hotmail.com>
2022-08-03 00:35:36 +0200

    Change to https for api call

--- data.tsx
+++ data.tsx
@@ -8,7 +8,7 @@ type WeatherData = {
 export const getData = async () => {
 
     let data: WeatherData = await fetch(
-        `http://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=Copenhagen&aqi=no`
+        `https://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=Copenhagen&aqi=no`
     );
 
     data = await data.json()
